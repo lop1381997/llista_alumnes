a361a4d5ac3ae3fc896190bad8c7fc1345e0a230
Civerneitor <sergio_castilla_13@hotmail.com>
2020-01-03 23:17:23 +0100

    Spinner?

--- app/src/main/java/com/example/llista_alumnes/CalendariActivity.java
+++ app/src/main/java/com/example/llista_alumnes/CalendariActivity.java
@@ -1,24 +1,14 @@
 package com.example.llista_alumnes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.CalendarView;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import androidx.annotation.NonNull;
-
-
-public class MainActivity extends Activity {
+public class CalendariActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +20,9 @@ public class MainActivity extends Activity {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                String date = i + "/" + (i1+1) + "/" + i2;
+                String date = i + "/" + i1 + "/" + i2;
 
-                Intent intent = new Intent(MainActivity.this, LlistaActivity.class);
+                Intent intent = new Intent(CalendariActivity.this, MainActivity.class);
                 intent.putExtra("date", date);
 
                 startActivity(intent);
---
+++ app/src/main/java/com/example/llista_alumnes/LlistaActivity.java
@@ -0,0 +1,90 @@
+package com.example.llista_alumnes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LlistaActivity extends Activity {
+    public final static int REQUEST_CODE_B = 1;
+    public final static int REQUEST_CODE_C = 2;
+
+    private TextView tv1;
+    private ListView lv1;
+    private Spinner spinner ;
+    private Toolbar toolbar;
+    private Custom_listview_adapter adapter;
+    private String date;
+
+
+    private ArrayList noms =new ArrayList <String> (Arrays.asList("Samuel", "Valentina", "Santiago", "Alejandro", "Valeria", "Benjamin",
+            "Gerardo", "Carlos", "David"));
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicialitza();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add_estudiants:
+                addEstudiant();
+                return true;
+                //case R.id.Llista
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void inicialitza() {
+        date = getIntent().getStringExtra("date");
+
+
+        tv1 = findViewById(R.id.tv1);
+        lv1 = findViewById(R.id.lv1);
+        toolbar = findViewById(R.id.toolbar);
+        if(date!=null){
+            toolbar.setTitle(date);
+        }
+
+        setActionBar(toolbar);
+        adapter = new Custom_listview_adapter(this,R.layout.adapter_list_view, R.id.lv1, noms);
+        lv1.setAdapter(adapter);
+
+    }
+
+    void addEstudiant(){
+        Intent intent = new Intent(getBaseContext(), second_activity.class );
+        startActivityForResult(intent, REQUEST_CODE_B);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case REQUEST_CODE_B:
+                noms.add(data.getStringExtra("name"));
+                adapter.notifyDataSetChanged();
+                break;
+
+        }
+    }
+}
